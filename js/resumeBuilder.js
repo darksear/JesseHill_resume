e2f60d60228c832b59258266c52ffc648a78d16d
darksear <darksear@live.com>
2015-09-13 21:43:02 -0700

    Initial Project

---
+++ js/resumeBuilder.js
@@ -0,0 +1,13 @@
+//Header
+var formattedName = HTMLheaderName.replace("%data%", "Jesse Hill");
+var formattedRole = HTMLheaderRole.replace("%data%", "Beginning Web Developer");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+//Work Experience
+
+
+//Projects
+
+
+//Education
